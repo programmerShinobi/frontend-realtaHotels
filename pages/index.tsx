b5d7aff42336d723c877692876c1b8442c83e164
FaQih <110642627+programmerShinobi@users.noreply.github.com>
2023-03-01 08:47:48 +0700

    v4.2 updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,16 +12,17 @@ export default function Home() {
         <title>Home</title>
       </Head>
       <Layout>
-        <Box className="grid shadow-md rounded-xl bg-white pb-8">
+        <Box className="grid shadow-lg shadow-orange-100 rounded-xl bg-white pb-8">
           {/* Home */}
           <Box className="m-8 pl-8 font-bold shadow-md w-full h-fit py-2 px-2 mx-auto items-center bg-orange-100 text-orange-900 hover:bg-orange-200 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-75">
             <Typography className={styles.textTitleInProfile}>
-              Home
+              Enjoy your holiday with us
             </Typography>
           </Box>
-          <Box className="pl-8 pr-8 text-left font-normal text-orange-900">
+          <Box className="pl-8 pr-8 font-normal text-orange-900 text-justify">
             <Typography className={styles.textLabelInProfile}>
-              This information will be display, so be careful what you share ...
+              Our hotel is located in the heart of the city, providing easy access to popular tourist attractions and business hubs.
+Enjoy unbeatable rates and special promotions when you book a room at our hotel.
             </Typography>
           </Box>
         </Box>
